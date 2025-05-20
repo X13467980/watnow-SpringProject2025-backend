1625fe85de9fbc6c86d0ce600aa253ff3b6a7370
Youta <0x59.xxx@gmail.com>
2025-05-20 15:22:40 +0900

    Fix: Rubocop style issues corrected

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post 'login', to: 'sessions#create'
-      delete 'logout', to: 'sessions#destroy'
+      post "login", to: "sessions#create"
+      delete "logout", to: "sessions#destroy"
     end
   end
-
 end
