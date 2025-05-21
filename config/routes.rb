086ce74b4259bf4d1888ea42634e395e46977473
Youta <0x59.xxx@gmail.com>
2025-05-22 03:17:24 +0900

    Feat: implement user registration endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "login", to: "sessions#create"
       delete "logout", to: "sessions#destroy"
+
+      post "signup", to: "users#create"
     end
   end
 end
