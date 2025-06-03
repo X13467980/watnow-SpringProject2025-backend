d34f54bfdb756fc5cf6d3158c1bf21cf67e73c71
Youta <0x59.xxx@gmail.com>
2025-06-03 09:42:47 +0900

    Fix: fix lint error by rubocop

--- app/controllers/api/v1/users_trainings_controller.rb
+++ app/controllers/api/v1/users_trainings_controller.rb
@@ -4,7 +4,7 @@ module Api
       def create
         training = UsersTraining.new(users_training_params)
         if training.save
-          render json: { message: '記録の作成に成功しました', training: training }, status: :created
+          render json: { message: "記録の作成に成功しました", training: training }, status: :created
         else
           render json: { errors: training.errors.full_messages }, status: :unprocessable_entity
         end
@@ -13,8 +13,8 @@ module Api
       def index
         trainings = UsersTraining.includes(:user, :menu).order(training_date: :desc)
         render json: trainings.as_json(include: {
-          user: { only: [:id, :name] },
-          menu: { only: [:id, :name, :part] }
+          user: { only: [ :id, :name ] },
+          menu: { only: [ :id, :name, :part ] }
         })
       end
 
