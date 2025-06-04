c616c59e8418d0812536b883aa7e4ece98be2803
Youta <0x59.xxx@gmail.com>
2025-06-05 02:35:36 +0900

    Feat: implement cookie-based login/logout with session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,13 @@
-# frozen_string_literal: true
-
 module Api
   module V1
     class SessionsController < ApplicationController
-      # ACでSCを作ってますよみたいな
+      # セッション系APIにはCSRFチェック無効にすることが多い
+      skip_before_action :verify_authenticity_token
+
       def create
         user = User.find_by(email: params[:email])
-
         if user&.authenticate(params[:password])
+          session[:user_id] = user.id
           render json: { message: "ログイン成功", user: user }, status: :ok
         else
           render json: { error: "メールアドレスまたはパスワードが間違っています" }, status: :unauthorized
@@ -15,7 +15,7 @@ module Api
       end
 
       def destroy
-        # フロントエンド側でトークン削除の処理を行う前提
+        reset_session
         render json: { message: "ログアウト成功" }, status: :ok
       end
     end
