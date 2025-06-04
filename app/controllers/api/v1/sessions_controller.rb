630b75a810147139373ee3fa89eab99bac17d3eb
Youta <0x59.xxx@gmail.com>
2025-06-05 02:40:32 +0900

    Fix: fix lint error by rubocop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class SessionsController < ApplicationController
-
       def create
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
