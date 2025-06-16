2667db7e6809b02721c26c10b0b88b00f592c41b
punksguitar <hanahanaogutaro@gmail.com>
2025-06-16 21:58:09 +0900

    FEAT:menuとmachine初期登録

--- db/migrate/20250614162145_add_memo_to_users_trainings.rb
+++
@@ -1,5 +0,0 @@
-class AddMemoToUsersTrainings < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users_trainings, :memo, :string
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,60 @@ menu2 = Menu.create!(name: "胸トレ", part: "胸", count: 12, set_count: 3, we
 MachineMenu.create!(machine: m1, menu: menu1)
 MachineMenu.create!(machine: m1, menu: menu2) # ラットプルダウンで胸トレもできると仮定
 
-User.create!(
-  id: 1,
-  name: "テストユーザー",
-  email: "test@example.com",
-  password: "password"
-)
+User.find_or_create_by!(email: "test3@example.com") do |user|
+  user.name = "テストユーザー"
+  user.password = "password"
+end
+
+machine_names = [
+  "ベンチプレス台",
+  "ラットプルマシン",
+  "レッグプレスマシン",
+  "スミスマシン",
+  "ショルダープレスマシン",
+  "ケーブルクロスマシン",
+  "ペックデック",
+  "アームカールマシン",
+  "レッグエクステンション",
+  "ヒップスラストマシン",
+  "腹筋ローラー",
+  "トレッドミル",
+  "ローイングマシン"
+]
+
+machine_names.each do |name|
+  Machine.find_or_create_by(name: name)
+end
+
+menu_seeds = [
+  { name: "ベンチプレス", part: "胸", machine_name: "ベンチプレス台" },
+  { name: "ラットプルダウン", part: "背中", machine_name: "ラットプルマシン" },
+  { name: "レッグプレス", part: "脚", machine_name: "レッグプレスマシン" },
+  { name: "スミスマシンスクワット", part: "脚", machine_name: "スミスマシン" },
+  { name: "ショルダープレス", part: "肩", machine_name: "ショルダープレスマシン" },
+  { name: "ケーブルクロス", part: "胸", machine_name: "ケーブルクロスマシン" },
+  { name: "ペックデック", part: "胸", machine_name: "ペックデック" },
+  { name: "アームカール", part: "腕", machine_name: "アームカールマシン" },
+  { name: "レッグエクステンション", part: "脚", machine_name: "レッグエクステンション" },
+  { name: "ヒップスラスト", part: "お尻", machine_name: "ヒップスラストマシン" },
+  { name: "腹筋ローラー", part: "腹筋", machine_name: "腹筋ローラー" },
+  { name: "トレッドミル走", part: "有酸素", machine_name: "トレッドミル" },
+  { name: "ローイング", part: "背中", machine_name: "ローイングマシン" }
+]
+
+menu_seeds.each do |menu_data|
+  machine = Machine.find_by(name: menu_data[:machine_name])
+  unless machine
+    puts "Machine not found: #{menu_data[:machine_name]}"
+    next
+  end
+
+  Menu.find_or_create_by!(
+    name: menu_data[:name],
+    part: menu_data[:part],
+    machine_id: machine.id,
+    count: nil,
+    set_count: nil,
+    time: nil
+  )
+end
