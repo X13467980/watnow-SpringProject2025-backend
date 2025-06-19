3c0d20495181346ef5fc1b053ef313287b4fbde5
Youta <0x59.xxx@gmail.com>
2025-06-19 16:25:58 +0900

    Feat: add flat bench & dumbbell machine and remove training detail fields from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 # db/seeds.rb
 m1 = Machine.create!(name: "ラットプルダウン", label: "Lat Pull Down, Pull-down", image_url: "lat.jpg")
 m2 = Machine.create!(name: "チェストプレス", label: "Chest Press", image_url: "chest.jpg")
+m3 = Machine.create!(name: "フラットベンチ＆ダンベル", label: "Flat Bench, Dumbbell, Free Weights, Bench Press", image_url: "bench.jpg")
 
-menu1 = Menu.create!(name: "背中トレ", part: "背中", count: 10, set_count: 3, weight: 40, machine_id: m1.id)
-menu2 = Menu.create!(name: "胸トレ", part: "胸", count: 12, set_count: 3, weight: 30, machine_id: m2.id)
+menu1 = Menu.create!(name: "背中トレ", part: "背中", machine_id: m1.id)
+menu2 = Menu.create!(name: "胸トレ", part: "胸", machine_id: m2.id)
+menu3 = Menu.create!(name: "ダンベルプレス", part: "胸", machine_id: m3.id)
+menu4 = Menu.create!(name: "サイドレイズ", part: "肩", machine_id: m3.id)
 
 MachineMenu.create!(machine: m1, menu: menu1)
 MachineMenu.create!(machine: m1, menu: menu2) # ラットプルダウンで胸トレもできると仮定
+MachineMenu.create!(machine: m3, menu: menu3)
+MachineMenu.create!(machine: m3, menu: menu4)
 
 User.create!(
   id: 1,
